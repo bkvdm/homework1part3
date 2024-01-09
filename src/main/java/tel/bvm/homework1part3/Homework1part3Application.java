15ef9bc4b12e21b862215d6a5b34a5ccff170127
bkvdm <vadim@991199.ru>
2024-01-09 18:19:18 +0300

    Домашняя работа №1 раздел 3: https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

---
+++ src/main/java/tel/bvm/homework1part3/Homework1part3Application.java
@@ -0,0 +1,15 @@
+package tel.bvm.homework1part3;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class Homework1part3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework1part3Application.class, args);
+	}
+
+}
---
+++ src/test/java/tel/bvm/homework1part3/Homework1part3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tel.bvm.homework1part3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework1part3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
