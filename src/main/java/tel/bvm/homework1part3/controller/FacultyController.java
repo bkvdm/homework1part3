e0d9c1cd0f089aec530a20e57bfa307bf70a05ec
bkvdm <vadim@991199.ru>
2024-01-09 23:02:43 +0300

    Домашняя работа №1 раздел 3: https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

--- src/main/java/tel/bvm/homework1part3/controller/FacultyController.java
+++ src/main/java/tel/bvm/homework1part3/controller/FacultyController.java
@@ -1,4 +1,35 @@
 package tel.bvm.homework1part3.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tel.bvm.homework1part3.model.Faculty;
+import tel.bvm.homework1part3.service.FacultyService;
+
 public class FacultyController {
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @PostMapping
+    public Faculty addFaculty(@RequestBody Faculty faculty) {
+        return facultyService.addFaculty(faculty);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Faculty> editFaculty(@PathVariable long id, @RequestBody Faculty faculty) {
+        Faculty foundFaculty = facultyService.editFaculty(id, faculty);
+        if (foundFaculty == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(foundFaculty);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteFaculty(@PathVariable long id) {
+        facultyService.deleteFaculty(id);
+        return ResponseEntity.ok().build();
+    }
 }
