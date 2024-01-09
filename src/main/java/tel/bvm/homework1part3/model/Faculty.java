1f1f2b78fb8673bb05ad0e393fa5e00dc082b052
bkvdm <vadim@991199.ru>
2024-01-09 19:12:26 +0300

    Домашняя работа №1 раздел 3: https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

---
+++ src/main/java/tel/bvm/homework1part3/model/Faculty.java
@@ -0,0 +1,57 @@
+package tel.bvm.homework1part3.model;
+
+import java.util.Objects;
+
+public class Faculty {
+    private long id;
+    private String name;
+    private String color;
+
+    public Faculty() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Faculty faculty)) return false;
+        return id == faculty.id && Objects.equals(name, faculty.name) && Objects.equals(color, faculty.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/tel/bvm/homework1part3/service/FacultyService.java
@@ -0,0 +1,4 @@
+package tel.bvm.homework1part3.service;
+
+public interface FacultyService {
+}
