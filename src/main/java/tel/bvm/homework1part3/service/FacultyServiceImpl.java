d8c6a0bd5ab48ab83b27eadf5bb8d3d67bf8ad32
bkvdm <vadim@991199.ru>
2024-01-12 22:36:28 +0300

    Домашняя работа №2 раздел 3: https://skyengpublic.notion.site/3-3-c3c2a6eb72b34d1b8e55ec214a3e26c9
    Домашняя работа №1 раздел 3:
    https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

--- src/main/java/tel/bvm/homework1part3/service/FacultyServiceImpl.java
+++ src/main/java/tel/bvm/homework1part3/service/FacultyServiceImpl.java
@@ -8,44 +8,30 @@ import java.util.List;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
-    //    @Autowired
+
     private FacultyRepository facultyRepository;
 
     public FacultyServiceImpl(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
     }
 
-//    private final HashMap<Long, Faculty> faculties = new HashMap<Long, Faculty>();
-//    private long count = 0;
-
-
     @Override
     public Faculty addFaculty(Faculty faculty) {
         return facultyRepository.save(faculty);
-//        faculty.setId(count++);
-//        faculties.put(faculty.getId(), faculty);
-//        return faculty;
     }
 
     @Override
     public Faculty findFaculty(long id) {
         return facultyRepository.findById(id).get();
-//        return faculties.get(id);
     }
 
     @Override
     public Faculty editFaculty(long id, Faculty faculty) {
-//        if (!faculties.containsKey(id)) {
-//            return null;
-//        }
-//        faculties.put(id, faculty);
-//        return faculty;
         return facultyRepository.save(faculty);
     }
 
     @Override
     public void deleteFaculty(long id) {
-//        faculties.remove(id);
         facultyRepository.deleteById(id);
 
     }
--- src/main/java/tel/bvm/homework1part3/service/StudentServiceImpl.java
+++ src/main/java/tel/bvm/homework1part3/service/StudentServiceImpl.java
@@ -8,57 +8,30 @@ import java.util.List;
 
 @Service
 public class StudentServiceImpl implements StudentService {
-    //    @Autowired
+
     private StudentRepository studentRepository;
 
     public StudentServiceImpl(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
-//    private final HashMap<Long, Student> students = new HashMap<Long, Student>();
-
-//    private long count = 0;
 
     @Override
     public Student addStudent(Student student) {
         return studentRepository.save(student);
-//        student.setId(count++);
-//        students.put(student.getId(), student);
-//        return student;
     }
-
-//    @Override
-//    public Optional<Student> findStudent(long id) {
-//        if (students.containsKey(id)) {
-//            return Optional.of(students.get(id));
-//        } else {
-//            return Optional.empty();
-//        }
-//    }
-
-//    @Override
-//    public Optional<Student> findStudent(long id) {
-//        return students.containsKey(id) ? Optional.of(students.get(id)) : Optional.empty();
-//    }
 
     @Override
     public Student findStudent(long id) {
-//        return students.get(id);
         return studentRepository.findById(id).get();
     }
 
     @Override
     public Student editStudent(long id, Student student) {
-//        if (!students.containsKey(id)) {
-//            return null;
-//        }
-//        students.put(id, student);
-//        return student;
         return studentRepository.save(student);
     }
 
     @Override
     public void deleteStudent(long id) {
-//        students.remove((id));
         studentRepository.deleteById(id);
     }
 
