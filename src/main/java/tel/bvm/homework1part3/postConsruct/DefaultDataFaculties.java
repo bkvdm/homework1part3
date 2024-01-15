6154dda346ad154da3961210f25e1a685f033b67
bkvdm <vadim@991199.ru>
2024-01-15 23:43:09 +0300

    Домашняя работа №3 раздел 3: https://skyengpublic.notion.site/3-4-SQL-44e31f0c3b48423fb4d824fadec60d1a
    Домашняя работа №2 раздел 3:
    https://skyengpublic.notion.site/3-3-c3c2a6eb72b34d1b8e55ec214a3e26c9
    
    Домашняя работа №1 раздел 3:
    https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

--- src/main/java/tel/bvm/homework1part3/postConsruct/DefaultDataFaculties.java
+++ src/main/java/tel/bvm/homework1part3/postConsruct/DefaultDataFaculties.java
@@ -1,4 +1,9 @@
 package tel.bvm.homework1part3.postConsruct;
 
 public class DefaultDataFaculties {
+
+//    1	Когневран (Ravenclaw)	Красный (Red)
+//            2	Гриффиндор (Gryffindor)	Жёлтый (Yellow)
+//            3	Пуффендуй (Hufflepuff)	Зелёный (Green)
+//            4	Слизерин (Slytherin)	Синий (Purple)
 }
--- src/main/java/tel/bvm/homework1part3/postConsruct/DefaultDataStudents.java
+++ src/main/java/tel/bvm/homework1part3/postConsruct/DefaultDataStudents.java
@@ -1,4 +1,31 @@
 package tel.bvm.homework1part3.postConsruct;
 
 public class DefaultDataStudents {
+
+//    1	Луна Лавгуд (Luna Lovegood)	25	Когневран (Ravenclaw)
+//2	Дин Томас (Dean Thomas)	78	Когневран (Ravenclaw)
+//3	Симус Финниган (Seamus Finnigan)	43	Пуффендуй (Hufflepuff)
+//4	Блейз Забини (Blaise Zabini)	67	Гриффиндор (Gryffindor)
+//5	Миллисента Булстроуд (Millicent Bulstrode)	89	Слизерин (Slytherin)
+//6	Панси Паркинсон (Pansy Parkinson)	93	Слизерин (Slytherin)
+//7	Чо Чанг (Cho Chang)	56	Слизерин (Slytherin)
+//8	Майкл Корнер (Michael Corner)	34	Когневран (Ravenclaw)
+//9	Роберт Хиллиард (Robert Hilliard)	100	Когневран (Ravenclaw)
+//10	Элеанор Брэнсток (Eleanor Branstock)	18	Когневран (Ravenclaw)
+//11	Кэти Белл (Katie Bell)	73	Пуффендуй (Hufflepuff)
+//12	Алисия Спиннет (Alicia Spinnet)	23	Гриффиндор (Gryffindor)
+//13	Анджелина Джонсон (Angelina Johnson)	64	Гриффиндор (Gryffindor)
+//14	Кэтти Бел (Kathy Bell)	83	Гриффиндор (Gryffindor)
+//15	Колин Криви (Colin Creevey)	49	Гриффиндор (Gryffindor)
+//16	Гарри Поттер (Harry Potter)	11	Гриффиндор (Gryffindor)
+//17	Гермиона Грейнджер (Hermione Granger)	11	Гриффиндор (Gryffindor)
+//18	Рон Уизли (Ron Weasley)	11	Гриффиндор (Gryffindor)
+//19	Драко Малфой (Draco Malfoy)	12	Гриффиндор (Gryffindor)
+//20	Невилл Лонгботтом (Neville Longbottom)	10	Слизерин (Slytherin)
+//21	Джинни Уизли (Ginny Weasley)	7	Пуффендуй (Hufflepuff)
+//22	Альбус Дамблдор (Albus Dumbledore)	115	Гриффиндор (Gryffindor)
+//23	Северус Снегг (Severus Snape)	38	Пуффендуй (Hufflepuff)
+//24	Филиус Флитвик (Filius Flitwick)	75	Когневран (Ravenclaw)
+
+
 }
