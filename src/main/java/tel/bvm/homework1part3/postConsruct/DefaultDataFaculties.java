4af1b1d1acbda637ead021b5c76584a16b78b042
bkvdm <vadim@991199.ru>
2024-01-16 22:28:29 +0300

    Домашняя работа №3 раздел 3: https://skyengpublic.notion.site/3-4-SQL-44e31f0c3b48423fb4d824fadec60d1a
    Домашняя работа №2 раздел 3:
    https://skyengpublic.notion.site/3-3-c3c2a6eb72b34d1b8e55ec214a3e26c9
    
    Домашняя работа №1 раздел 3:
    https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

--- src/main/java/tel/bvm/homework1part3/postConsruct/DefaultDataFaculties.java
+++ src/main/java/tel/bvm/homework1part3/postConsruct/DefaultDataFaculties.java
@@ -3,17 +3,26 @@ package tel.bvm.homework1part3.postConsruct;
 import tel.bvm.homework1part3.model.Faculty;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class DefaultDataFaculties {
 
+    private final DefaultDataStudents defaultDataStudents;
+
+    public DefaultDataFaculties(DefaultDataStudents defaultDataStudents) {
+        this.defaultDataStudents = defaultDataStudents;
+    }
+
     public Faculty facultyInfo (long id) {
 
-        Faculty faculty1 = new Faculty(1, "Когневран (Ravenclaw)", "Красный (Red)", );
-        Faculty faculty2 = new Faculty(2, "Гриффиндор (Gryffindor)", "Жёлтый (Yellow)", "й");
-        Faculty faculty3 = new Faculty(3, "Пуффендуй (Hufflepuff)", "Зелёный (Green)", "й");
-        Faculty faculty4 = new Faculty(4, "Слизерин (Slytherin)", "Фиолетовый (Purple)", "й");
+        Faculty faculty1 = new Faculty(1, "Когневран (Ravenclaw)", "Красный (Red)", null);
+        faculty1.setStudents(defaultDataStudents.studentList(faculty1));
+        Faculty faculty2 = new Faculty(2, "Гриффиндор (Gryffindor)", "Жёлтый (Yellow)", null);
+        faculty2.setStudents(defaultDataStudents.studentList(faculty2));
+        Faculty faculty3 = new Faculty(3, "Пуффендуй (Hufflepuff)", "Зелёный (Green)", null);
+        faculty3.setStudents(defaultDataStudents.studentList(faculty3));
+        Faculty faculty4 = new Faculty(4, "Слизерин (Slytherin)", "Фиолетовый (Purple)", null);
+        faculty4.setStudents(defaultDataStudents.studentList(faculty4));
 
         Map<Long, Faculty> facultyMap = new HashMap<>();
         facultyMap.put(faculty1.getId(), faculty1);
