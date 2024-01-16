69085f9d6782c355045767d406b4d4bdcb1357c7
bkvdm <vadim@991199.ru>
2024-01-16 22:53:45 +0300

    Домашняя работа №3 раздел 3: https://skyengpublic.notion.site/3-4-SQL-44e31f0c3b48423fb4d824fadec60d1a
    Домашняя работа №2 раздел 3:
    https://skyengpublic.notion.site/3-3-c3c2a6eb72b34d1b8e55ec214a3e26c9
    
    Домашняя работа №1 раздел 3:
    https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

--- src/main/java/tel/bvm/homework1part3/postConsruct/TransferDatabase.java
+++ src/main/java/tel/bvm/homework1part3/postConsruct/TransferDatabase.java
@@ -1,38 +1,52 @@
 package tel.bvm.homework1part3.postConsruct;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import tel.bvm.homework1part3.model.Student;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class TransferDatabase {
 
-//    @Autowired
-//    private DataSource dataSource; // предположим, что соединение с базой уже создано
-//
-//
-//    @PostConstruct
-//    void init() {
-//        insertData();
-//    }
-//
-//
-//    void insertData() {
-//        final Connection connection = dataSource.getConnection();
-//        try(PreparedStatement statement = connection.prepareStatement(
-//                "INSERT INTO STUDENTS(ID, NAME, AGE) VALUES(?, ?, ?)")){
-//
-//            for(Student student: students){ // students - список студентов
-//                int id = student.getId();
-//                String name = student.getName();
-//                int age = student.getAge();
-//
-//                statement.setInt(1, id);
-//                statement.setString(2, name);
-//                statement.setInt(3, age);
-//                statement.addBatch();
-//            }
-//
-//            statement.executeBatch();
-//        }catch (SQLException e){
-//            throw new RuntimeException(e);
-//        }finally{
-//            connection.close();
-//        }
-//    }
+    private DefaultDataStudents defaultDataStudents;
+
+    public TransferDatabase(DefaultDataStudents defaultDataStudents) {
+        this.defaultDataStudents = defaultDataStudents;
+    }
+
+    @Autowired
+    private DataSource dataSource; // предположим, что соединение с базой уже создано
+
+    @PostConstruct
+    void init() throws SQLException {
+        insertData();
+    }
+
+    void insertData() throws SQLException {
+        final Connection connection = dataSource.getConnection();
+
+        try(PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO STUDENTS(ID, NAME, AGE) VALUES(?, ?, ?)")){
+
+            for(Student student: defaultDataStudents.studentList){ // students - список студентов
+                long id = student.getId();
+                String name = student.getName();
+                int age = student.getAge();
+
+                statement.setLong(1, id);
+                statement.setString(2, name);
+                statement.setInt(3, age);
+                statement.addBatch();
+            }
+
+            statement.executeBatch();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }finally{
+            connection.close();
+        }
+    }
 }
