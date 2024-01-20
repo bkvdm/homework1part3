b505011c35e1b3b947d6f7ddd3078bc361c52739
bkvdm <vadim@991199.ru>
2024-01-20 19:53:50 +0300

    Домашняя работа №3 раздел 3: https://skyengpublic.notion.site/3-4-SQL-44e31f0c3b48423fb4d824fadec60d1a
    Домашняя работа №2 раздел 3:
    https://skyengpublic.notion.site/3-3-c3c2a6eb72b34d1b8e55ec214a3e26c9
    
    Домашняя работа №1 раздел 3:
    https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

--- src/test/java/tel/bvm/homework1part3/service/FacultyServiceImplTest.java
+++ src/test/java/tel/bvm/homework1part3/service/FacultyServiceImplTest.java
@@ -5,20 +5,30 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 import tel.bvm.homework1part3.model.Faculty;
 import tel.bvm.homework1part3.repository.DataConstants;
 import tel.bvm.homework1part3.repository.FacultyRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static tel.bvm.homework1part3.repository.DataConstants.*;
 
 @ExtendWith(MockitoExtension.class)
 class FacultyServiceImplTest {
 
     @InjectMocks
-    FacultyService facultyServiceOut;
+    FacultyServiceImpl facultyServiceImpl;
 
     @InjectMocks
-    StudentService studentServiceOut;
+    StudentServiceImpl studentServiceImpl;
 
     @Mock
     FacultyRepository facultyRepository;
@@ -27,14 +37,28 @@ class FacultyServiceImplTest {
     void setUp() {
 
 
+
     }
 
     @Test
-    void addFaculty() {
+    void addFacultyVerify() {
+        Mockito.when(facultyRepository.save(FACULTY_4)).thenReturn(FACULTY_4);
+        facultyServiceImpl.addFaculty(FACULTY_4);
+        Mockito.verify(facultyRepository, Mockito.times(1)).save(FACULTY_4);
+        assertEquals(FACULTY_4, facultyServiceImpl.addFaculty(FACULTY_4));
     }
 
     @Test
-    void findFaculty() {
+    void findFacultyVerify() {
+        Mockito.when(facultyRepository.findById(3L)).thenReturn(Optional.ofNullable(FACULTY_3));
+        assertEquals(Optional.ofNullable(FACULTY_3), facultyRepository.findById(3L));
+        Mockito.verify(facultyRepository, Mockito.times(1)).findById(3L);
+    }
+
+    @Test
+    void findFacultyExceptionVerify() {
+        Mockito.when(facultyRepository.findById(5L)).thenThrow(HttpClientErrorException.NotFound.class);
+        assertThrows(HttpClientErrorException.NotFound.class, () -> facultyRepository.findById(5L));
     }
 
     @Test
