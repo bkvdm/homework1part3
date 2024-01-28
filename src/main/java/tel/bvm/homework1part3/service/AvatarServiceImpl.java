383f4c8c6a0ae09be30367af368a12afc8ea6d7d
bkvdm <vadim@991199.ru>
2024-01-28 11:07:08 +0300

    Домашняя работа №3 раздел 3: https://skyengpublic.notion.site/3-4-SQL-44e31f0c3b48423fb4d824fadec60d1a
    Домашняя работа №2 раздел 3:
    https://skyengpublic.notion.site/3-3-c3c2a6eb72b34d1b8e55ec214a3e26c9
    
    Домашняя работа №1 раздел 3:
    https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

--- src/main/java/tel/bvm/homework1part3/service/AvatarServiceImpl.java
+++ src/main/java/tel/bvm/homework1part3/service/AvatarServiceImpl.java
@@ -4,12 +4,19 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import tel.bvm.homework1part3.model.Avatar;
 import tel.bvm.homework1part3.model.Student;
 import tel.bvm.homework1part3.repository.AvatarRepository;
 import tel.bvm.homework1part3.repository.StudentRepository;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 @Transactional
@@ -29,6 +36,21 @@ public class AvatarServiceImpl implements AvatarService{
     public void upLoadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
         Student student = studentRepository.getById(studentId);
         Path filePath = Path.of(avatarDir, student + "." + getExceptions(avatarFile.getOriginalFilename()));
-
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+        try (
+                InputStream is = avatarFile.getInputStream();
+                OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+                BufferedInputStream bis = new BufferedInputStream(is, 1024)
+        ) {
+            bis.transferTo(bos);
+        }
+        Avatar avatar = findAvatar(studentId);
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.getFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.getData(generateDataForDB(filePath));
+        avatarRepository.save(avatar);
     }
 }
