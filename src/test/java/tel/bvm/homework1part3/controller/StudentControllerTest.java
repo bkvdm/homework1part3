b49d9562339ea3f1056e5ebc47d35316523badca
bkvdm <vadim@991199.ru>
2024-02-05 20:38:27 +0300

    Домашняя работа №4 раздел 3: https://skyengpublic.notion.site/3-6-Spring-Boot-0070e5697e594bd0a5c6e5f96a29f950
    Домашняя работа №2 раздел 3:
    https://skyengpublic.notion.site/3-3-c3c2a6eb72b34d1b8e55ec214a3e26c9
    
    Домашняя работа №1 раздел 3:
    https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

--- src/test/java/tel/bvm/homework1part3/controller/StudentControllerTest.java
+++ src/test/java/tel/bvm/homework1part3/controller/StudentControllerTest.java
@@ -6,6 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import tel.bvm.homework1part3.model.Student;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static tel.bvm.homework1part3.repository.DataConstants.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentControllerTest {
@@ -22,5 +29,17 @@ public class StudentControllerTest {
     @Test
     void contextLoadsStudent() throws Exception{
         Assertions.assertThat(studentController).isNotNull();
+    }
+
+    @Test
+    public void testGetStudentInfo() {
+        ResponseEntity<Student> response = restTemplate.getForEntity("http://localhost:" + port + "/student/" + STUDENT_1.getId(), Student.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        Student student = response.getBody();
+        assertNotNull(student);
+        assertEquals(STUDENT_1.getId(), student.getId());
+        assertEquals(STUDENT_1.getName(), student.getName());
+        assertEquals(STUDENT_1.getAge(), student.getAge());
     }
 }
