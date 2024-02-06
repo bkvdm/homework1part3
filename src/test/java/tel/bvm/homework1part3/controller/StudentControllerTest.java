44ff8a55ea3fdd207d25e69aebdbf737869a6f43
bkvdm <vadim@991199.ru>
2024-02-06 16:21:54 +0300

    Домашняя работа №4 раздел 3: https://skyengpublic.notion.site/3-6-Spring-Boot-0070e5697e594bd0a5c6e5f96a29f950
    Домашняя работа №2 раздел 3:
    https://skyengpublic.notion.site/3-3-c3c2a6eb72b34d1b8e55ec214a3e26c9
    
    Домашняя работа №1 раздел 3:
    https://my.sky.pro/student-cabinet/stream-lesson/100300/homework

--- src/test/java/tel/bvm/homework1part3/controller/StudentControllerTest.java
+++ src/test/java/tel/bvm/homework1part3/controller/StudentControllerTest.java
@@ -15,6 +15,7 @@ import tel.bvm.homework1part3.model.Student;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static tel.bvm.homework1part3.repository.DataConstants.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,7 +32,7 @@ public class StudentControllerTest {
     private StudentController studentController;
 
     @Test
-    void contextLoadsStudent() throws Exception{
+    void contextLoadsStudent() throws Exception {
         Assertions.assertThat(studentController).isNotNull();
     }
 
@@ -49,11 +50,33 @@ public class StudentControllerTest {
 
     @Test
     public void testGetAllStudents() {
-        ResponseEntity<List<Student>> response = restTemplate.exchange("http://localhost:" + port + "/student", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {});
+        ResponseEntity<List<Student>> response = restTemplate.exchange("http://localhost:" + port + "/student", HttpMethod.GET, null, new ParameterizedTypeReference<List<Student>>() {
+        });
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         List<Student> students = response.getBody();
         assertNotNull(students);
         assertEquals(24, students.size());
     }
+
+//    @Test
+//    public void testFindByAgeBetween() {
+//        ResponseEntity<List<Student>> response = restTemplate.exchange(
+//                "http://localhost:" + port + "/student/findByAgeBetween?from=20&to=25",
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<Student>>() {
+//                });
+//
+//        List<Student> students = response.getBody();
+//
+//        // Добавим проверку на то, что список студентов не пустой
+//        assertFalse(students.isEmpty());
+//
+//        // Проверка на соответствие полученных данных ожидаемым
+//        // Например, можно проверить, что все студенты в списке действительно имеют возраст от 20 до 25 лет
+//        for (Student student : students) {
+//            assertTrue(student.getAge() >= 20 && student.getAge() <= 25);
+//        }
+//    }
 }
