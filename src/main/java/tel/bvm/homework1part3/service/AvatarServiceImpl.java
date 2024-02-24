5df28d7cdf58158f0be813dba3b349c3a281edeb
bkvdm <vadim@991199.ru>
2024-02-24 16:18:55 +0300

    Домашняя работа №5 раздел 3: https://my.sky.pro/student-cabinet/stream-lesson/75320/homework-requirements

--- src/main/java/tel/bvm/homework1part3/service/AvatarServiceImpl.java
+++ src/main/java/tel/bvm/homework1part3/service/AvatarServiceImpl.java
@@ -1,6 +1,8 @@
 package tel.bvm.homework1part3.service;
 
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
@@ -37,8 +39,11 @@ public class AvatarServiceImpl implements AvatarService {
         this.studentRepository = studentRepository;
     }
 
+    Logger logger = LoggerFactory.getLogger(AvatarServiceImpl.class);
+
     @Override
     public void upLoadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        logger.info("Method upLoadAvatar in file name: " + avatarFile + " student with id: " + studentId);
         Student student = studentRepository.getReferenceById(studentId);
 //        Optional<Student> student = studentRepository.findById(studentId);
         Path filePath = Path.of(avatarDir, student.getName() + "." + getExtensions(avatarFile.getOriginalFilename()));
@@ -71,12 +76,15 @@ public class AvatarServiceImpl implements AvatarService {
 
     //    @Override
     private byte[] generateDataForDB(Path filePath) throws IOException {
+        logger.info("Generating data for database from file: " + filePath.getFileName());
         try (InputStream is = Files.newInputStream(filePath);
              BufferedInputStream bis = new BufferedInputStream(is, 1024);
              ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             BufferedImage image = ImageIO.read(bis);
 
             int height = image.getHeight() / (image.getWidth() / 100);
+            logger.debug("Height of the original image: " + image.getHeight());
+            logger.debug("Width of the original image: " + image.getWidth());
             BufferedImage preview = new BufferedImage(100, height, image.getType());
             Graphics2D graphics2D = preview.createGraphics();
             graphics2D.drawImage(image, 0, 0, 100, height, null);
@@ -89,15 +97,18 @@ public class AvatarServiceImpl implements AvatarService {
 
     @Override
     public Avatar findAvatar(Long studentId) {
+        logger.info("Method findAvatar has been called with studentId: " + studentId);
         return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
     }
 
     private String getExtensions(String fileName) {
+        logger.info("Method getExtensions has been called for file: " + fileName);
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     @Override
     public List<Avatar> getAllAvatar(Integer pageNumber, Integer pageSize) {
+        logger.info("Method getAllAvatar has been called with pageNumber: " + pageNumber + " and pageSize: " + pageSize);
         PageRequest pageRequest = PageRequest.of(pageNumber - 1, pageSize);
         return avatarRepository.findAll(pageRequest).getContent();
     }
--- src/main/java/tel/bvm/homework1part3/service/FacultyServiceImpl.java
+++ src/main/java/tel/bvm/homework1part3/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package tel.bvm.homework1part3.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -19,43 +21,53 @@ public class FacultyServiceImpl implements FacultyService {
         this.facultyRepository = facultyRepository;
     }
 
+    Logger logger = LoggerFactory.getLogger(FacultyServiceImpl.class);
+
     @Override
     public Faculty addFaculty(Faculty faculty) {
+        logger.info("Adding faculty: " + faculty.getName());
         return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty findFaculty(long id) {
+        logger.info("Finding faculty with id: " + id);
         return facultyRepository.findById(id).orElse(null);
     }
 
     @Override
     public Faculty editFaculty(Long id, Faculty faculty) {
+        logger.info("Editing faculty with id: " + id);
         return facultyRepository.save(faculty);
     }
 
     @Override
     public void deleteFaculty(long id) {
+        logger.info("Deleting faculty with id: " + id);
         facultyRepository.deleteById(id);
     }
 
     @Override
     public List<Faculty> findAllFaculties() {
+        logger.info("Finding all faculties");
         return facultyRepository.findAll();
     }
 
     @Override
     public List<Faculty> findByNameContainingIgnoreCase(String name) {
+        logger.info("Finding faculties by name containing: " + name);
         return facultyRepository.findByNameContainingIgnoreCase(name);
     }
 
     @Override
     public List<Faculty> findByColorContainingIgnoreCase(String color) {
+        logger.info("Finding faculties by color containing: " + color);
         return facultyRepository.findByColorContainingIgnoreCase(color);
     }
 
     @Override
     public ResponseEntity<List<Faculty>> findByNameAndColorContainingIgnoreCase(String name, String color) {
+        logger.info("Method findByNameAndColorContainingIgnoreCase called");
         if (name != null && color != null) {
             if (color.isBlank() && name.isBlank()) {
                 return ResponseEntity.ok(facultyRepository.findAll());
@@ -66,12 +78,16 @@ public class FacultyServiceImpl implements FacultyService {
             } else {
                 return ResponseEntity.ok(facultyRepository.findByNameAndColorContainingIgnoreCase(name, color));
             }
-        } else return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } else
+            logger.error("Faculty not found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @Override
     public List<Student> findByFacultyOfStudent(Long id, String name, String color) {
+        logger.info("Method findByFacultyOfStudent is called with parameters id: {}, name: {}, color: {}", id, name, color);
         if (Optional.ofNullable(facultyRepository.findByIdOrNameOrColorContainingIgnoreCase(id, name, color)).isEmpty()) {
+            logger.error("Students of the specified faculty were not found");
             ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         return facultyRepository.findByIdOrNameOrColorContainingIgnoreCase(id, name, color).getStudents();
